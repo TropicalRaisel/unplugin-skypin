cdf20c92984f6f5e22b190e5b1e159ddb4bce4b6
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-10 16:56:56 -0700

    Fixed future tests

--- test/building/__test__/build.test.ts
+++ test/building/__test__/build.test.ts
@@ -3,29 +3,17 @@ import fs from 'fs-extra'
 
 const r = (...args: string[]) => resolve(__dirname, '../dist', ...args)
 
+async function performTest(file: string) {
+  const content = await fs.readFile(r(file), 'utf-8')
+  expect(content).toContain('import \'hueman\'')
+  expect(content).toContain('import \'https://cdn.skypack.dev/pin/hueman@v2.1.3-5b8y8npx5feiJPqgqL57/mode=imports,min/optimized/hueman.js\'')
+}
+
 describe('inject skypack', () => {
-  it('vite', async() => {
-    const content = await fs.readFile(r('vite/main.js.es.js'), 'utf-8')
-
-    expect(content).toContain('NON-TARGET: hueman')
-    expect(content).toContain('TARGET: [Injected Vite]')
-  })
-
-  it('rollup', async() => {
-    const content = await fs.readFile(r('rollup/main.js'), 'utf-8')
-
-    expect(content).toContain('NON-TARGET: hueman')
-    expect(content).toContain('TARGET: [Injected Rollup]')
-  })
-
-  it('webpack', async() => {
-    const content = await fs.readFile(r('webpack/main.js'), 'utf-8')
-
-    expect(content).toContain('NON-TARGET: hueman')
-    expect(content).toContain('TARGET: [Injected Webpack]')
-  })
-})
- */
+  it('vite', async() => performTest('vite/main.js.es.js'))
+  it('rollup', async() => performTest('rollup/main.js'))
+  it('webpack', async() => performTest('webpack/main.js'))
+}) */
 
 describe('index', () => {
   it('hi jest', () => {
