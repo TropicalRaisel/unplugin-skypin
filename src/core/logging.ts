d44400cd9e731bb365ae19bfc48f2dfc1c273a1c
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-09 20:03:12 -0700

    Added logging helper

---
+++ src/core/logging.ts
@@ -0,0 +1,11 @@
+import { Category, CategoryServiceFactory, CategoryLogger, CategoryConfiguration, LogLevel } from 'typescript-logging'
+
+// Optionally change default settings, in this example set default logging to Info.
+// Without changing configuration, categories will log to Error.
+CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info))
+
+// Create categories, they will autoregister themselves, one category without parent (root) and a child category.
+const infoLogger = new Category('skypin-info')
+
+// Optionally get a logger for a category, since 0.5.0 this is not necessary anymore, you can use the category itself to log.
+export const log: CategoryLogger = CategoryServiceFactory.getLogger(infoLogger)
