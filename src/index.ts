0d9d6a9132323718e8fa520a672acd7e78a111d2
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-11 00:09:05 -0700

    Decreased build times by preprocessing urls

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,29 @@ import { resolveOptions } from './core/options'
 export default createUnplugin<Options>((options, meta) => {
   const settings = resolveOptions(options)
   const name = 'unplugin-skypin'
+  const urls = new Map<string, string>()
 
   return {
     name,
-    // https://rollupjs.org/guide/en/#build-hooks
-    // https://vitejs.dev/guide/api-plugin.html#plugin-ordering
     enforce: 'pre',
-    async resolveId(id: string) {
-      if (!settings.packages.includes(id))
-        return id
+    async buildStart() {
+      for (const id of settings.packages) {
+        const sub = settings.replace(id)
 
-      if (settings.replace) {
-        // https://github.com/MarshallCB/skypin/blob/main/src/index.ts#L49
-        const url = await skypin(id, {
-          min: settings.minified,
-          pin: settings.pinned,
-        })
-
+        if (sub) {
+          urls.set(typeof sub === 'string' ? sub : id, await skypin(id, {
+            min: settings.minified,
+            pin: settings.pinned,
+          }))
+        }
+      }
+    },
+    resolveId(id: string) {
+      if (urls.has(id)) {
+        const url = urls.get(id)!
         return meta.framework !== 'webpack' ? { id: url, external: true } : url
       }
+      return id
     },
   }
 })
--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,5 @@ export interface Options {
   packages: string[]
   minified: boolean
   pinned: boolean
-  replace: boolean
+  replace: (id: string) => (boolean | string)
 }
