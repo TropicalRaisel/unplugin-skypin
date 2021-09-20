22cc934a6ab563a867d2cdb15e75ef8b318f55b6
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-20 10:11:11 -0700

    Cleaned up logger

--- src/core/log.ts
+++ src/core/log.ts
@@ -1,11 +1,6 @@
-import { Category, CategoryServiceFactory, CategoryLogger, CategoryConfiguration, LogLevel } from 'typescript-logging'
+import { Category, CategoryServiceFactory, CategoryConfiguration, LogLevel } from 'typescript-logging'
 
-// Optionally change default settings, in this example set default logging to Info.
-// Without changing configuration, categories will log to Error.
 CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info))
 
-// Create categories, they will autoregister themselves, one category without parent (root) and a child category.
-const genericLogger = new Category('skypin')
-
-// Optionally get a logger for a category, since 0.5.0 this is not necessary anymore, you can use the category itself to log.
-export const log: CategoryLogger = CategoryServiceFactory.getLogger(genericLogger)
+const category = new Category('skypin')
+export const log = CategoryServiceFactory.getLogger(category)
