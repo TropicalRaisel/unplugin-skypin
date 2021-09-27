46fefc0634c75051b2932897d8218bdf35ba38cc
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-27 09:38:19 -0700

    Now using Skylib!

--- src/core/log.ts
+++
@@ -1,6 +0,0 @@
-import { Category, CategoryServiceFactory, CategoryConfiguration, LogLevel } from 'typescript-logging'
-
-CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info))
-
-const category = new Category('skypin')
-export const log = CategoryServiceFactory.getLogger(category)
